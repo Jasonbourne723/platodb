1555b776c5c30c262a2c78d6e027578877813c72
Zaijainlixiangtl <420994592@qq.com>
2024-11-09 09:57:57 +0800

    fix:代码冲突

--- internal/database/sstable/sstable.go
+++ internal/database/sstable/sstable.go
@@ -1,14 +1,5 @@
 package sstable
 
-const (
-	_  = iota
-	KB = 1 << (10 * iota)
-	MB = 1 << (10 * iota)
-	GB = 1 << (10 * iota)
-
-	BLOCK_SIZE = 32 * KB
-)
-
 type SSTable struct {
 	Segments []*Segment
 }
